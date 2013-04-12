28eac0bb6f88d39afb7c6ce183f73e084aa6578a
Patrik Wenger <paddor@gmail.com>
2013-04-12 20:21:09 +0200

    remove comment

--- lib/em-simple_telnet.rb
+++ lib/em-simple_telnet.rb
@@ -870,9 +870,6 @@ class EventMachine::Protocols::SimpleTelnet < EventMachine::Connection
   # Call this method *only* if <tt>@connection_state_callback</tt> is set!
   #
   def call_connection_state_callback(obj=nil)
-    # FIXME: maybe this is needed to avoid FiberError's (dead fiber called).
-#    return if @connection_state_callback.nil?
-
     callback, @connection_state_callback = @connection_state_callback, nil
     callback.call(obj)
   end
